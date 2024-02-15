e3c4238af05f7eaadfbaf48eadc63a96812ad811
Stephenie <stephenie.oboh@ucalgary.ca>
2024-02-14 20:40:18 -0700

    began inquirer and reliefservice implementation

---
+++ GP3/edu/ucalgary/oop/Inquirer.java
@@ -0,0 +1,31 @@
+package GP3.edu.ucalgary.oop;
+
+public class Inquirer {
+    private String firstName;
+    private String lastName;
+    private String info;
+    private String servicesPhoneNum;
+
+    public Inquirer(String firstName, String lastName, String servicesPhoneNum, String info) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.servicesPhoneNum = servicesPhoneNum;
+        this.info = info;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getServicesPhoneNum() {
+        return servicesPhoneNum;
+    }
+}
---
+++ GP3/edu/ucalgary/oop/ReliefService.java
@@ -0,0 +1,70 @@
+package GP3.edu.ucalgary.oop;
+
+public class ReliefService {
+     private Inquirer inquirer;
+    private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        if (inquirer == null || missingPerson == null || dateOfInquiry == null ||
+            infoProvided == null || lastKnownLocation == null) {
+            throw new IllegalArgumentException("Arguments cannot be null");
+        }
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = dateOfInquiry;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
+
+    public void setInquirer(Inquirer inquirer) {
+        this.inquirer = inquirer;
+    }
+
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    public void setDateOfInquiry(String dateOfInquiry) {
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public void setInfoProvided(String infoProvided) {
+        this.infoProvided = infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(Location lastKnownLocation) {
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public String getLogDetails() {
+        // Assuming log details include all information
+        return "Inquirer: " + inquirer + "\n" +
+               "Missing Person: " + missingPerson + "\n" +
+               "Date of Inquiry: " + dateOfInquiry + "\n" +
+               "Info Provided: " + infoProvided + "\n" +
+               "Last Known Location: " + lastKnownLocation;
+    }
+}
