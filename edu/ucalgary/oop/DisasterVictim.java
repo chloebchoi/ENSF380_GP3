04505dd7ab6c8bda22b6f9b38e8a1ba652069363
Evan Mann <ejmann900@gmail.com>
2024-02-15 20:17:03 -0700

    Got all files to compile

--- edu/ucalgary/oop/DisasterVictim.java
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -1,7 +1,8 @@
 package edu.ucalgary.oop;
+
 import java.util.Arrays;
 
-public class DisasterVictim{
+public class DisasterVictim {
     private String firstName;
     private String lastName;
     private String dateOfBirth;
@@ -14,75 +15,87 @@ public class DisasterVictim{
     private Supply[] personalBelongings;
     private int counter;
 
-
     // constructor
     public DisasterVictim(String firstName, String ENTRY_DATE) {
         this.firstName = firstName;
-        this. ENTRY_DATE = ENTRY_DATE;
+        this.ENTRY_DATE = ENTRY_DATE;
         // constructor must throw exception
     }
-    
 
     // getters
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public String getDateOfBirth() {
         return dateOfBirth;
     }
+
     public String getComments() {
         return comments;
     }
-    public MedicalRecords[] getMedicalRecords() {
+
+    public MedicalRecord[] getMedicalRecords() {
         return medicalRecords;
     }
+
     public String getEntryDate() {
         return ENTRY_DATE;
     }
+
     public int getAssignedSocialID() {
         return ASSIGNED_SOCIAL_ID;
     }
+
     public Supply[] getPersonalBelongings() {
         return personalBelongings;
     }
+
     public FamilyRelation[] getFamilyConnections() {
         return familyConnections;
     }
+
     public String getGender() {
         return gender;
     }
 
-    
     // setters
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
         // must throw exception here
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
-    public void setComments(String comments){
+
+    public void setComments(String comments) {
         this.comments = comments;
     }
+
     public void setMedicalRecords(MedicalRecord[] medicalRecords) {
         this.medicalRecords = medicalRecords;
     }
+
     public void setPersonalBelongings(Supply[] supplies) {
         this.personalBelongings = supplies;
     }
+
     public void setFamilyConnections(FamilyRelation[] relation) {
         this.familyConnections = relation;
     }
-    
 
     // other methods
     public void addPersonalBelonging(Supply supply) {
@@ -91,28 +104,32 @@ public class DisasterVictim{
         temp[x] = supply;
         this.personalBelongings = temp;
     }
+
     public void removePersonalBelonging(Supply supply) {
         int x = personalBelongings.length;
         Supply[] temp = Arrays.copyOf(personalBelongings, x - 1);
-        for (int i = 0; i < (x-1); i++){
+        for (int i = 0; i < (x - 1); i++) {
             temp[i] = personalBelongings[i];
         }
         this.personalBelongings = temp;
     }
+
     public void addFamilyConnection(FamilyRelation familyConnection) {
         int x = familyConnections.length;
         FamilyRelation[] temp = Arrays.copyOf(familyConnections, x + 1);
         temp[x] = familyConnection;
         this.familyConnections = temp;
     }
+
     public void removeFamilyConnection(FamilyRelation familyConnection) {
         int x = familyConnections.length;
         FamilyRelation[] temp = Arrays.copyOf(familyConnections, x - 1);
-        for (int i = 0; i < (x-1); i++){
+        for (int i = 0; i < (x - 1); i++) {
             temp[i] = familyConnections[i];
         }
         this.familyConnections = temp;
     }
+
     public void addMedicalRecord(MedicalRecord medicalRecord) {
         int x = medicalRecords.length;
         MedicalRecord[] temp = Arrays.copyOf(medicalRecords, x + 1);
